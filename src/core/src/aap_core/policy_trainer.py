d4085924d239aea1b154207600ed6d1a86ddd836
Ly Hon Quang <lyhonquang@gmail.com>
2026-05-06 15:43:37 +0700

    copy PPO from TEMPERA for comparison

--- src/core/src/aap_core/policy_trainer.py
+++ src/core/src/aap_core/policy_trainer.py
@@ -468,3 +468,120 @@ class ReinforcePP(BasePolicyTrainer):
             wandb.finish()
         print("done")
 
+
+class PPO(BasePolicyTrainer):
+    def __init__(
+        self,
+        actor_critic: BasePolicy,
+        env: PromptOptimizationEnv,
+        max_episodes: int,
+        optimizer: torch.optim.Optimizer,
+        lr_scheduler: torch.optim.lr_scheduler.LRScheduler,
+        clip_param,
+        num_mini_batch,
+        value_loss_coef,
+        entropy_coef,
+        exploration_module=None,
+        replay_buffer=None,
+        max_grad_norm: float = 1.0,
+        use_clipped_value_loss=True,
+        eps=1e-8,
+    ):
+        super().__init__(
+            actor_critic,
+            env,
+            max_episodes,
+            optimizer,
+            lr_scheduler,
+            exploration_module,
+            replay_buffer,
+            eps,
+        )
+        self.clip_param = clip_param
+        self.num_mini_batch = num_mini_batch
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
+        self.max_grad_norm = max_grad_norm
+        self.use_clipped_value_loss = use_clipped_value_loss
+
+    def fit(self, **kwargs):
+        advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
+        advantages = (advantages - advantages.mean()) / (advantages.std() + self.eps)
+        value_loss_epoch = 0.0
+        action_loss_epoch = 0.0
+        dist_entropy_epoch = 0.0
+
+        for e in range(self.max_episodes):
+            if self.policy_model.is_recurrent:
+                data_generator = rollouts.recurrent_generator(
+                    advantages, self.num_mini_batch
+                )
+            else:
+                data_generator = rollouts.feed_forward_generator(
+                    advantages, self.num_mini_batch
+                )
+
+            for sample in data_generator:
+                (
+                    obs_batch,
+                    recurrent_hidden_states_batch,
+                    actions_batch,
+                    value_preds_batch,
+                    return_batch,
+                    masks_batch,
+                    old_action_log_probs_batch,
+                    adv_targ,
+                ) = sample
+
+                # Reshape to do in a single forward pass for all steps
+                values, action_log_probs, dist_entropy, _ = (
+                    self.policy_model.evaluate_actions(
+                        obs_batch,
+                        actions_batch,
+                        masks_batch,
+                    )
+                )
+
+                ratio = torch.exp(action_log_probs - old_action_log_probs_batch)
+                surr1 = ratio * adv_targ
+                surr2 = (
+                    torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param)
+                    * adv_targ
+                )
+                action_loss = -torch.min(surr1, surr2).mean()
+
+                if self.use_clipped_value_loss:
+                    value_pred_clipped = value_preds_batch + (
+                        values - value_preds_batch
+                    ).clamp(-self.clip_param, self.clip_param)
+                    value_losses = (values - return_batch).pow(2)
+                    value_losses_clipped = (value_pred_clipped - return_batch).pow(2)
+                    value_loss = (
+                        0.5 * torch.max(value_losses, value_losses_clipped).mean()
+                    )
+                else:
+                    value_loss = 0.5 * (return_batch - values).pow(2).mean()
+
+                self.optimizer.zero_grad()
+                (
+                    value_loss * self.value_loss_coef
+                    + action_loss
+                    - dist_entropy * self.entropy_coef
+                ).backward()
+                nn.utils.clip_grad_norm_(
+                    self.policy_model.parameters(), self.max_grad_norm
+                )
+                self.optimizer.step()
+
+                value_loss_epoch += value_loss.item()
+                action_loss_epoch += action_loss.item()
+                dist_entropy_epoch += dist_entropy.item()
+
+        num_updates = self.max_episodes * self.num_mini_batch
+
+        value_loss_epoch /= num_updates
+        action_loss_epoch /= num_updates
+        dist_entropy_epoch /= num_updates
+
+        return value_loss_epoch, action_loss_epoch, dist_entropy_epoch
+
